cc341562eab04b8cac1b623030567e06137a51c0
jeth318 <jesper.thornberg@me.com>
2023-01-06 10:02:47 +0100

    Add emojis

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DayPickerWrapper from "./components/DayPickerWrapper"
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DayPickerWrapper from "./components/DayPickerWrapper";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hur många dagar är det mellan?</h1>
+      <h1>Hur jävla många dagar?</h1>
+      <h3>
+        Välj datum nedan och se hur många dagar det är från start- till
+        slutdatum.
+      </h3>
+      <hr />
       <DayPickerWrapper />
     </div>
   );
--- src/components/DayPickerWrapper.tsx
+++ src/components/DayPickerWrapper.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React from "react";
 import { useEffect } from "react";
 
-import { format } from 'date-fns';
-import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
+import { format } from "date-fns";
+import { DayPicker } from "react-day-picker";
+import { useMediaPredicate } from "react-media-hook";
 
-import CSS from 'csstype';
+import "react-day-picker/dist/style.css";
 
-const wrapperStyles: CSS.Properties = {
+import CSS from "csstype";
+
+const wrapperStyles: (md: boolean) => CSS.Properties = (md: boolean) => ({
   display: "flex",
-  flexDirection: "row",
+  minWidth: "320px",
+  flexDirection: md ? "column" : "row",
   justifyContent: "center",
-};
+});
 
-const daysBetweenStyles: CSS.Properties = {
+const daysBetweenStyles: (md: boolean) => CSS.Properties = (md: boolean) => ({
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
   alignItems: "center",
-  fontSize: "40px"
-};
+  fontSize: md ? "30px" : "40px",
+  padding: "10px",
+});
+
+const vhrStyles: (md: boolean) => CSS.Properties = (md: boolean) => ({
+  borderTop: md ? "3px solid grey" : "unset",
+  borderLeft: md ? "unset" : "3px solid grey",
+  width: md ? "auto" : "0",
+  height: md ? "0" : "auto",
+  margin: "20px",
+});
 
 export default function DayPickerWrapper() {
   const DAY_IN_MS = 1000 * 3600 * 24;
@@ -27,45 +39,65 @@ export default function DayPickerWrapper() {
   const [selectedEnd, setEndSelected] = React.useState<Date>();
   const [days, setDays] = React.useState<number>();
 
+  const isMediumScreen = useMediaPredicate("(max-width: 680px)");
+
   let footerStart = <p>Välj ett startdatum.</p>;
   let footerEnd = <p>Välj ett slutdatum.</p>;
   if (selectedStart) {
-    footerStart = <p>Du valde startdatumet {format(selectedStart, 'PP')}.</p>;
+    footerStart = <p>Du valde startdatumet {format(selectedStart, "PP")}.</p>;
   }
 
   if (selectedEnd) {
-    footerEnd = <p>Du valde slutdatumet {format(selectedEnd, 'PP')}.</p>;
+    footerEnd = <p>Du valde slutdatumet {format(selectedEnd, "PP")}.</p>;
   }
 
-  const onDateSelection = function() {
+  const onDateSelection = function () {
     if (selectedStart && selectedEnd) {
       const msDiff = selectedEnd.getTime() - selectedStart.getTime();
-      setDays(Math.abs(msDiff / DAY_IN_MS))
+      console.log(Math.round(Math.abs(msDiff / DAY_IN_MS)));
+      setDays(Math.round(Math.abs(msDiff / DAY_IN_MS)));
     }
-  }
+  };
 
-  useEffect(onDateSelection, [selectedStart, selectedEnd])
+  useEffect(onDateSelection, [selectedStart, selectedEnd, DAY_IN_MS]);
+  const renderDays = function () {
+    if (days !== undefined) {
+      if (days === 0) {
+        return <div>👏🧠💀🧌</div>;
+      } else {
+        return <div>{days} dagar 🤌</div>;
+      }
+    }
 
+    if (!selectedStart && !selectedEnd) {
+      return <div>👇 Välj datumspann 👇</div>;
+    }
+    if (selectedStart && !selectedEnd) {
+      return <div>Välj datum till 👇</div>;
+    }
+    if (!selectedStart && selectedEnd) {
+      return <div>👇 Välj datum från</div>;
+    }
+  };
   return (
     <>
-      <div style={wrapperStyles}>
-      <DayPicker
-        mode="single"
-        selected={selectedStart}
-        onSelect={setStartSelected}
-        footer={footerStart}
-      />
-      <DayPicker
-      mode="single"
-      selected={selectedEnd}
-      onSelect={setEndSelected}
-      footer={footerEnd}
-    />
-    
-    </div>
-    <div style={daysBetweenStyles}>
-      {days ? `${days} dagar` : "Välj dagar"}
-    </div>
+      <div style={daysBetweenStyles(isMediumScreen)}>{renderDays()}</div>
+      <hr />
+      <div style={wrapperStyles(isMediumScreen)}>
+        <DayPicker
+          mode="single"
+          selected={selectedStart}
+          onSelect={setStartSelected}
+          footer={footerStart}
+        />
+        <div style={vhrStyles(isMediumScreen)}></div>
+        <DayPicker
+          mode="single"
+          selected={selectedEnd}
+          onSelect={setEndSelected}
+          footer={footerEnd}
+        />
+      </div>
     </>
   );
 }
